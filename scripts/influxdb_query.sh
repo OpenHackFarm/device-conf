de1afacf6891577590f3b95998f23c4bb6eabb34
yan <a0726h77@gmail.com>
2018-08-06 06:20:10 +0800

    Add script

--- scripts/influxdb_query.sh
+++ scripts/influxdb_query.sh
@@ -1,12 +1,21 @@
 HOST=localhost
+PRETTY=true
 
 # Query last 60 min
-curl -G "http://$HOST:8086/query?pretty=true" \
+curl -G "http://$HOST:8086/query?pretty=$PRETTY" \
   --data-urlencode "db=test" \
   --data-urlencode "q=SELECT \"soil_temperature_c\", \"VWC\" FROM \"test\" WHERE \"id\"='4DAD64' AND time > now() - 60m"
 
+echo '\n################################################\n'
+
 # Query last 20 row
-curl -G "http://$HOST:8086/query?pretty=true" \
+curl -G "http://$HOST:8086/query?pretty=$PRETTY" \
   --data-urlencode "db=test" \
   --data-urlencode "q=SELECT \"soil_temperature_c\", \"VWC\" FROM \"test\" WHERE \"id\"='4DAD64' ORDER BY time DESC LIMIT 20"
 
+echo '\n################################################\n'
+
+# Query last data
+curl -G "http://$HOST:8086/query?pretty=$PRETTY" \
+  --data-urlencode "db=test" \
+  --data-urlencode "q=SELECT * FROM \"AgriSensors\" WHERE \"name\"='OHF-Davis' ORDER BY time DESC LIMIT 1"
