dc42d57def1e2e8f0a7d6a9157acd56d1f31ab57
yan <a0726h77@gmail.com>
2018-08-13 22:47:53 +0800

    Update data comment

--- miflora_test.py
+++ miflora_test.py
@@ -6,6 +6,6 @@ DEV_ID = 'MIFLORA_DUMMY'
 
 URL = 'https://api.ohf.ag/iot/data'
 
-sensor_data = [26.2, 59, 1970, 1376, 21]  # 光照, 水分, 溫度, 肥力, 電力
+sensor_data = [26.2, 59, 1970, 1376, 21]  # 溫度, 土壤溼度, 光照, 電導度, 電力 https://github.com/open-homeautomation/miflora/blob/master/demo.py
 
 r = requests.post(URL, json={"id": DEV_ID, "data": sensor_data})
